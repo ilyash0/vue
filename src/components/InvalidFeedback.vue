85208fd7a2d95b6e3dd4afe30a7466061340238d
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2026-01-23 10:39:20 +0500

    fix

--- src/components/InvalidFeedback.vue
+++ src/components/InvalidFeedback.vue
@@ -1,9 +1,9 @@
 <script setup>
 defineProps({
-  error: Object,
+  errors: Object,
 })
 </script>
 
 <template>
-  <span class="invalid-feedback" v-if="error">{{error[0]}}</span>
+  <span class="invalid-feedback" v-if="errors">{{errors[0]}}</span>
 </template>
