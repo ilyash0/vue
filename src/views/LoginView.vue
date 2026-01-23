9d3dd56b6f4b587e8b440a7ce1d3f41e8e5f8c64
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2026-01-23 10:59:39 +0500

    update login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,18 +7,50 @@ export default {
 }
 </script>
 
+<script setup>
+import {ref} from "vue";
+import apiFetch from "@/helpers/apiFetch";
+import InvalidFeedback from "@/components/InvalidFeedback.vue";
+
+const form = ref({
+  data: {
+    name: '',
+    email: '',
+    password: '',
+  },
+  errors: {},
+  isSending: false,
+})
+
+const sendForm = async () => {
+  if (form.value.isSending) return;
+  form.value.errors = {}
+
+  form.value.isSending = true;
+
+  const result = await apiFetch('post', '/authorization', form.value.data)
+  if (result.errors) {
+    form.value.errors = result.errors
+  }
+  form.value.isSending = false
+}
+</script>
+
 <template>
   <div class="container d-flex justify-content-center align-items-center min-vh-100">
     <div class="card shadow p-4" style="max-width: 400px; width: 100%;">
       <h4 class="mb-4 text-center">Вход</h4>
-      <form>
+      <form @submit.prevent="sendForm()" novalidate>
         <div class="mb-3">
           <label>Email</label>
-          <input type="email" class="form-control" required>
+          <input type="email" v-model="form.data.email" class="form-control" :class="{'is-invalid': form.errors?.name}">
+          <InvalidFeedback :errors="form.errors?.email"/>
         </div>
         <div class="mb-3">
           <label>Пароль</label>
-          <input type="password" class="form-control" required>
+          <input type="password" v-model="form.data.password" class="form-control"
+                 :class="{'is-invalid': form.errors?.password}">
+          <InvalidFeedback :errors="form.errors?.password"/>
         </div>
         <button type="submit" class="btn btn-primary w-100">Войти</button>
         <div class="text-center mt-3">
